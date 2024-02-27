3de602e908ea1365839ec2c5530e8d12344c45d6
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-02-27 10:49:24 -0700

    login/logout

--- javatest.js
+++ javatest.js
@@ -1,3 +1,11 @@
 console.log("hi")
 
 // Branch 1 change
+function login(){
+    // do login here
+    console.log("logging in")
+}
+function logout(){
+    // do logout here
+    console.log("Logging out")
+}
