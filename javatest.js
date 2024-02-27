ba6a10bf573dd77a481871f2b0246db88256428c
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-02-27 10:51:32 -0700

    update/logout

---
+++ javatest.js
@@ -0,0 +1,11 @@
+// In branch 2
+
+function updateUser(){
+    //updating user
+    console.log("update User")
+}
+
+function logout(){
+    // My shitty logout
+    console.log("DEFINITELY NOT LOGGING OUT")
+}
